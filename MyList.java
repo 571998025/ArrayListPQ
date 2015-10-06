3767edcc8195488a99a5d6e86ace686c343c8a37
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-10-06 16:01:24 +0700

    init starter code

---
+++ MyList.java
@@ -0,0 +1,9 @@
+public interface MyList {
+	public void add(Object element);
+	public void remove(Object element);
+	public boolean isEmpty();
+	public boolean contains(Object element);
+	public int size();
+	dpublic Object get(int i);
+}
+
